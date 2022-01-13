440950e962a8ffcb4ae9da8d41270a5130b8372c
enar <nishchhalraturi@gmail.com>
2022-01-13 23:54:44 +0530

    basic changes

---
+++ notes_app/target/rls/debug/deps/notes_app-5bf8803d683bd0e8.d
@@ -0,0 +1,5 @@
+/home/enar/projects/Rust_projects/notes_app/target/rls/debug/deps/notes_app-5bf8803d683bd0e8.rmeta: src/main.rs
+
+/home/enar/projects/Rust_projects/notes_app/target/rls/debug/deps/notes_app-5bf8803d683bd0e8.d: src/main.rs
+
+src/main.rs:
---
+++ notes_app/target/rls/debug/deps/notes_app-7e0a8337cf3bcf39.d
@@ -0,0 +1,5 @@
+/home/enar/projects/Rust_projects/notes_app/target/rls/debug/deps/notes_app-7e0a8337cf3bcf39.rmeta: src/main.rs
+
+/home/enar/projects/Rust_projects/notes_app/target/rls/debug/deps/notes_app-7e0a8337cf3bcf39.d: src/main.rs
+
+src/main.rs:
